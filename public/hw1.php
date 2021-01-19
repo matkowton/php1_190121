0e5aae99c749cc97ef58c954d703443e93e64911
matkowton <matkowton@gmail.com>
2021-01-19 21:56:08 +0300

    init commit

---
+++ public/hw1.php
@@ -0,0 +1,3 @@
+<h1>Lesson 1</h1>
+<p>Hello, world!</p>
+<p>2021</p>
---
+++ public/index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ $a = 5;
+$b = &$a;
+$b = $b + 1;
+var_dump($a, $b);*/
+/*
+define('PI', 3.14);
+
+var_dump(PI);
+*/
+
+// Типы Данных
+/*
+$integer = 10;
+$float = 0.75;
+$string_single = 'это \n\t строка';
+$string_double = "это {$integer} тоже строка";
+$boolean = true; //false
+
+$array = [1, 2, 6, 0];
+$object = new stdClass();
+//$resource = fopen('test.txt', 'r');
+
+$null = null;
+
+$a = 5;
+$b = '0';
+
+$x = $a + $b;
+
+var_dump((bool) $b);
+*/
+
+//Операции со строками
+/*
+$string1  = "Hello";
+//$string1 = $string1 . 'world';
+$string1 .= 'world';
+*/
+
+//Математические операции
+
+/*
+$a = 5;
+$b = 7;
+echo $a + $b;
+echo $a - $b;
+echo $a * $b;
+echo $a / $b;
+echo $a % $b;
+echo $a ** $b;
+
+var_dump($a++);
+
+$a += 5;*/
+
+//Операции сравнениия
+/*
+$a = 5;
+$b = '5';
+
+var_dump($a == $b);
+var_dump($a === $b);
+var_dump($a != $b);
+var_dump($a !== $b);
+var_dump($a > $b);
+var_dump($a < $b);
+var_dump($a >= $b);
+var_dump($a <= $b);
+
+var_dump($a <=> $b);*/
+
+var_dump(null >= 0);
+
